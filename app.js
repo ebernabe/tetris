850ac39e7abce5318990a809693065138266c030
Ernesto Bernabe <ernes.bs@gmail.com>
2013-03-30 00:38:47 -0600

    adduser

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
-
+var bandera_usuario="";
 var app = express();
+var users_count = new Array(2);
 
 var mtio=new Array(24);//matrix tetris
 for(i=0;i<=23;i++)
@@ -20,7 +21,26 @@ for(i=0;i<=23;i++)
     }
 }
 
-
+function adduser(user){
+  var bandera = 0;
+  var msg = "no mas usuarios";
+   if((users_count[0]=="" || users_count[0]==user) && bandera==0 ){users_count[0]=user; bandera =1;}
+   if((users_count[1]=="" || users_count[1]==user) && bandera==0 ){users_count[1]=user; bandera =1;}
+ if(bandera){
+    console.log(user);
+ }else{
+     console.log(msg);
+    /* qq=setTimeout(function(){
+        if(bandera_usuario!=""){
+                if(users_count[0]!=bandera_usuario){users_count[0]=""; bandera_usuario="";}
+                if(users_count[1]!=bandera_usuario){users_count[1]=""; bandera_usuario="";}
+        }else{
+            bandera_usuario=user;
+        }
+     },1000);*/
+ }
+ 
+}
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -52,6 +72,7 @@ io.sockets.on('connection', function(socket){
   socket.emit('connected'); 
   socket.on('tetrisr', function(data,fic,fil,vel,user){
       console.log(data);
+      adduser(user);
       //socket.emit('omt'); 
       //envia los datos del tetris a los demas X jugadores sera para 2 o mas en el futuro
       socket.broadcast.emit("omt",data,fic,fil,vel,user);
